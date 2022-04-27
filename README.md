d9a366515423ed3498d8a472c602f4004afb741e
torvista <4391026+torvista@users.noreply.github.com>
2022-04-27 08:25:17 +0200

    update: see changelog

--- README.md
+++ README.md
@@ -1,20 +1,43 @@
-# zen-cart_backup_MySQL_auto
-Script to backup a Zen Cart database via a cron job
-Tested with Zen Cart 1.56a on PHP 7.3.
-Uses only the admin configure.php (local or normal) from the Zen Cart fileset so should be compatible with any version.
+# Zen Cart - Backup MySQL auto/cron
+Script to backup a Zen Cart database via a cron job.
+
+Compatible with all versions Zen Cart. Requires minimum php 7.4.  
+Since it uses only the admin version configure.php (local or production) from the Zen Cart fileset it should be compatible with any version.
+
 Install in /YOURSHOPFOLDER/YOURADMINFOLDER/cgi-bin/
 
-cron examples
-check with your host for the correct php path to use
+You may test it from the browser:  
+
+www.yourshopaddress/YOURADMINFOLDER/cgi-bin/backup_mysql_cron.php
+
+Maybe it will work out of the box, otherwise if errors occur, set $debug = true near the start of the script to debug.
+
+Read the info in the script to help with possible changes you may have to make.
+
+Probably the only thing you may have to modify will be the path to the mysqldump.exe, which creates the backup file.
+
+You may report issues in Github.
+
+https://github.com/torvista/Zen_Cart-Backup_MySQL_auto/issues
+
+
+### cron examples
+
+Often a lot of trial and error is necessary. Check with your host for the correct php path to use
 1) normal:
 /usr/local/bin/php -q /home/USERNAME/public_html/SHOP/ADMINFOLDER/cgi-bin/backup_mysql_cron.php
 
 2) if exec is disabled by default, but can be enabled by a php.ini in the script directory:
 /usr/local/bin/php -c /home/USERNAME/public_html/SHOP/ADMINFOLDER//cgi-bin/php.ini -q /home/USERNAME/public_html/SHOP/ADMINFOLDERS//cgi-bin/backup_mysql_cron.php
 
-Changelog
-1.3 2019 03 04: modified to require less configuration
-1.2 2019 02 27: extra debugging/error messages. script should be run from admin directory. Uploaded to plugins
-1.1 2016 08 28: updated to cope with special chars in the database password 
-1.0 2008 - based on  http://www.zen-cart.com/forum/showthread.php?t=106666
+## Changelog
+1.4 - 2022 04 27: fix for mysql 8 --column-statistics=0, fix for gz file not being created/dumpfile .sql not being deleted, strict mode, compatible with php 8, more debugging info.
+
+1.3 - 2019 03 04: modified to require less configuration
+
+1.2 - 2019 02 27: extra debugging/error messages. script should be run from admin directory. Uploaded to plugins
+
+1.1 - 2016 08 28: updated to cope with special chars in the database password
+
+1.0 - 2008: based on  http://www.zen-cart.com/forum/showthread.php?t=106666
 
--- files/YOURADMIN FOLDER/cgi-bin/backup_mysql_cron.php
+++ files/YOURADMIN FOLDER/cgi-bin/backup_mysql_cron.php
@@ -1,20 +1,19 @@
 <?php
+declare(strict_types=1);
 //place in /YOURSHOPFOLDER/YOURADMIN/FOLDER/cgi-bin/
 //backup database script for Zen Cart
-//torvista 04/03/2019
+//torvista 27/04/2022
 //based on http://www.zen-cart.com/forum/showthread.php?t=106666
-
-//INSTALLATION
-//Put this file in the shop ADMIN directory and rename it/add some random suffix for extra security
-
-//USE
-//The script can be run from the browser (on demand/cron) or from the command line (cron).
 
 //DEBUGGING
 //false: default setting. Displays minimum confirmation text for cron result email.
-//true: for debugging. Note that it will show the MySQL database PASSWORD.
+//true: for debugging only. Note that it will show the MySQL database PASSWORD.
 $debug = false;
-
+if ($debug) {
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
+}
 //CONFIGURATION
 
 //HOSTING - Production Server
@@ -24,13 +23,10 @@ $mysqltool_remote = '/usr/bin/mysqldump';
 //Local/Development Server
 //If you do not have a development server where you test modifications like this script, you are very silly indeed!
 //add the COMPLETE path to the mysqldump executable to this array. I used an array so it will work in different test environments
-$mysqltool_local =
-    array(
-        'c:/xampp.5.6.31/mysql/bin/mysqldump.exe',
-        'c:/xampp.7.1.26/mysql/bin/mysqldump.exe',
-        'c:/xampp.7.2.11/mysql/bin/mysqldump.exe',
-        'c:/xampp.7.3.0/mysql/bin/mysqldump.exe'
-    );
+$mysqltool_local = array(
+    'c:/xampp.7.3.3/mysql/bin/mysqldump.exe',
+    'C:/laragon/bin/mysql/mysql-8.0.27-winx64/bin/mysqldump.exe'
+);
 /*****************************************************************************/
 //script needs timezone set for correct date in backup filename
 if (date_default_timezone_get()) {
@@ -41,14 +37,15 @@ if (date_default_timezone_get()) {
 //initialise variables
 $error = false;
 $mysqltool = '';
-define('OS_DELIM_WIN', '"');
-define('OS_DELIM_NIX', "'");
+$mysql_version = 0;
+const OS_DELIM_WIN = '"';
+const OS_DELIM_NIX = "'";
 $slash = DIRECTORY_SEPARATOR;
 $path_to_admin = str_replace($slash . 'cgi-bin', '', __DIR__);
-(stristr(PHP_OS,
-    "win") ? $os_delim = OS_DELIM_WIN : $os_delim = OS_DELIM_NIX);//when password has special chars, windows and *nix need different delimiters or you get a mysqldump error 2 when access is refused for the bad password
+(stripos(PHP_OS_FAMILY, "win") !== false ? $os_delim = OS_DELIM_WIN
+    : $os_delim = OS_DELIM_NIX);//when password has special chars, windows and *nix need different delimiters or you get a mysqldump error 2 when access is refused for the bad password
 //is script being run from the browser (so use html for display) or via cron (don't use html tags for more readable confirmation email)
-$cron_shell = isset($_SERVER['SERVER_NAME']) ? false : true;
+$cron_shell = !isset($_SERVER['SERVER_NAME']);
 $lf = ($cron_shell ? "\n" : "<br>\n");//to avoid cron result email status being littered with html tags
 
 $redirect = '';//sends to other place IF debug not set also
@@ -63,12 +60,14 @@ if (!$debug && $redirect) {
  * Based on function by Kioob at:
  * http://www.php.net/manual/en/function.gzwrite.php#34955
  *
- * @param string $source Path to file that should be compressed
+ * @param string  $source Path to file that should be compressed
  * @param integer $level GZIP compression level (default: 9)
- * @return string New filename (with .gz appended) if success, or false if operation fails
+ *
+ * @return bool|string New filename (with .gz appended) if success, or false if operation fails
  */
-function gzCompressFile($source, $level = 9, $debug)
+function gzCompressFile(string $source, int $level = 9)
 {
+    global $debug, $lf;
     $dest = $source . '.gz';
     $mode = 'wb' . $level;
     $error = false;
@@ -86,9 +85,25 @@ function gzCompressFile($source, $level = 9, $debug)
         $error = true;
     }
     if ($error) {
+        if ($debug) {
+            echo "gzCompressFile: ERROR$lf";
+        }
         return false;
+    }
+
+    if ($debug) {
+        echo "gzCompressFile: gz file created:$lf $dest $lf";
+    }
+
+    if (unlink($source)) {
+        if ($debug) {
+            echo "gzCompressFile: source .sql dump file deleted$lf";
+        }
     } else {
-        unlink($source);
+        echo "gzCompressFile: ERROR gz compressed file NOT deleted$lf";
+        if ($debug) {
+            echo "$dest $lf";
+        }
     }
     return $dest;
 }
@@ -103,15 +118,14 @@ if ($debug && !$cron_shell) { ?>
             body {
                 font-family: Arial, Helvetica, sans-serif;
                 font-size: 12px;
-                line-height: 1.5;
             }
 
             h1 {
-                font-size: 16px;
+                font-size: 14px;
             }
 
             h2 {
-                font-size: 14px;
+                font-size: 13px;
             }
         </style>
     </head>
@@ -126,7 +140,9 @@ if ($debug) {
 
 // check to see if "exec()" is disabled in PHP -- if so, won't be able to use this tool.
 $php_disabled_functions = @ini_get("disable_functions");
-if (in_array('exec', preg_split('/,/', str_replace(' ', '', $php_disabled_functions)))) {
+//steve ide suggestion to test
+// if (in_array('exec', preg_split('/,/', str_replace(' ', '', $php_disabled_functions)))) {
+if (in_array('exec', explode(",", str_replace(' ', '', $php_disabled_functions)), true)) {
     echo " $lf ERROR! exec not available: this script cannot run. $lf";
     echo "PHP directive: disable_functions=$php_disabled_functions $lf";
     echo "Contact your host for possible fixes.$lf";
@@ -147,11 +163,14 @@ if (file_exists($configure_file = $path_to_admin . $slash . 'includes' . $slash 
     foreach ($mysqltool_local as $value) {
         if (file_exists($value)) {
             $mysqltool = $value;
+            $mysql_version = (int)(mysqli_get_client_version()/10000);
             if ($debug) {
                 echo "mysqldump found at:$mysqltool $lf";
+                echo "MySQL version=" . mysqli_get_client_version() . $lf;
+                echo '$mysql_version=' . $mysql_version . $lf;
             }
         }
-        if ($mysqltool != '') {
+        if ($mysqltool !== '') {
             break;
         }
     }
@@ -169,8 +188,8 @@ if (file_exists($configure_file = $path_to_admin . $slash . 'includes' . $slash 
     if (!file_exists($mysqltool)) {
         echo 'mysqldump NOT FOUND' . ($debug ? " at :$mysqltool" : '') . $lf;
         $error = true;
-    } else {
-        if ($debug) echo "mysqldump found at:$mysqltool $lf";
+    } elseif ($debug) {
+        echo "mysqldump found at:$mysqltool $lf";
     }
 } else {
     echo "ERROR! NO configuration file found $lf";
@@ -178,12 +197,11 @@ if (file_exists($configure_file = $path_to_admin . $slash . 'includes' . $slash 
 }
 
 if (!$error) {
-
     $backup_path = $path_to_admin . $slash . 'backups' . $slash;
     $backup_filename = 'db_' . DB_DATABASE . '-' . date('Y-m-d_H-i-s') . '_auto.sql';//name of the backup file
     $backup_dump = $backup_path . $backup_filename;
-
-    $dump_params = ' "--host=' . DB_SERVER . '"';
+    $dump_params = $mysql_version >= 8 ? ' --column-statistics=0 ' : ''; //default parameter in mysqldump 8 https://serverfault.com/questions/912162/mysqldump-throws-unknown-table-column-statistics-in-information-schema-1109
+    $dump_params .= ' "--host=' . DB_SERVER . '"';
     $dump_params .= ' "--user=' . DB_SERVER_USERNAME . '"';
     $dump_params .= ' --password=' . $os_delim . DB_SERVER_PASSWORD . $os_delim;//NIX DEFINITELY needs single quotes around the filename when shell metacharacters *%&$& etc. are in the password
     $dump_params .= ' --opt';
@@ -198,20 +216,28 @@ if (!$error) {
 
     exec($command, $output, $return_dump);
 
-    if ($return_dump === 0) {//success on 0
-        echo "SUCCESS!$lf";
+    if ($return_dump === 0) {//success on 0, which is an integer
         if ($debug) {
             echo "mysqdump executed$lf command=$lf" . ($cron_shell ? $command : htmlspecialchars($command)) . $lf;
+            echo ".sql dumpfile created ok$lf";
         }
 
         if (!file_exists($backup_dump)) {
-            echo "ERROR! Backup dumpfile does not exist:$lf $backup_filename $lf";
-        } elseif ($debug) {
-            echo "dumpfile exists:$lf $backup_dump $lf";
+            $error = true;
+            echo "ERROR! .sql dumpfile NOT FOUND$lf";
+            if ($debug) {
+                echo "$backup_filename $lf";
+            }
+
+        } else {
+            if ($debug) {
+                echo "dumpfile exists:$lf $backup_dump $lf";
+            }
         }
 
     } else {//mysqldump returned an error
-        echo "ERROR! BACKUP FILE NOT CREATED" . (!$debug ? ': set debug=true in cron script for details' : '') . $lf;
+        $error = true;
+        echo "ERROR: .sql dump file NOT CREATED" . (!$debug ? ': set debug=true in cron script for details' : '') . $lf;
         if ($debug) {
             echo "command=$lf" . ($cron_shell ? $command : htmlspecialchars($command)) . $lf;
             echo "exec return value ==$lf";
@@ -226,23 +252,24 @@ if (!$error) {
                 echo '</pre>';
             }
         }
-        $error = true;
     }
 
 //compress file
-    if (!$error) {
-        $backup_dump = gzCompressFile($backup_dump, '', $debug);
-
-        if ($backup_dump != false) {//gzip created
+    if ($error) {
+        echo "gz file NOT created due to previous error$lf";
+    } else {
+        $backup_dump = gzCompressFile($backup_dump);
+        if ($backup_dump === false) {//gzip not created
+            echo "ERROR: .sql.gz file was NOT created from .sql dumpfile$lf";
+        } else { //all ok
+            echo "gz file created ok: " . str_replace($backup_path, '', $backup_dump) . $lf . $lf;
             if ($debug) {
-                echo "GZipped file created:$lf $backup_dump $lf Script completed";
-            } else {//function returned false = fail
-                echo "...GZipped file created: " . str_replace($backup_path, '', $backup_dump) . $lf;
+                echo "Script Completed ok";
             }
-        } else {//gzip NOT created
-            echo "ERROR! .sql.gz file NOT created from .sql file $lf";
         }
     }
+} else {
+    echo "Script Aborted$lf";
 }
 
 ?>
